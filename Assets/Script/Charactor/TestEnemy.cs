120d3655f2b092a2e246cbf28799a2c96fa6968f
Bidoumyouou <moriyanotenagaasinagasama@yahoo.co.jp>
2018-09-28 16:03:41 +0900

    escape

--- Assets/Script/Charactor/TestEnemy.cs
+++ Assets/Script/Charactor/TestEnemy.cs
@@ -57,7 +57,7 @@ public class TestEnemy : Charactor
 
         
         //エネミーUIの作成
-        StartCoroutine("SetEnemyUI");
+        //StartCoroutine("SetEnemyUI");
         //EnemyUI = GameMgr.thisobject.EnemyUI;
 
     }
