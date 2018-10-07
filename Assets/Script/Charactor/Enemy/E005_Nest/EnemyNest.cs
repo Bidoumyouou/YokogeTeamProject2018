b2c22675d4b92ad9d2dc06873c47c4739a32ac58
Bidoumyouou <moriyanotenagaasinagasama@yahoo.co.jp>
2018-10-07 16:58:15 +0900

    EnemyNestの追加

---
+++ Assets/Script/Charactor/Enemy/E005_Nest/EnemyNest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[System.Serializable]
+public class NestParameter : System.Object {
+    public float frequency;
+    public GameObject SpawnEnemyPrefab;
+    public int maxspawn;
+}
+
+
+public class EnemyNest : TestEnemy
+{
+    public int spawned_n = 0;
+    public NestParameter param;
+    //ネストの基本ステータス
+
+
+    // Use this for initialization
+    void Start()
+    {
+        StartEnemy();
+        ChangeMode(FirstMode);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();
+
+    }
+}
