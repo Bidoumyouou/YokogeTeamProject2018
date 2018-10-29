b554578113ed1422d73b08b0c0d3680dab6aa101
Bidoumyouou <moriyanotenagaasinagasama@yahoo.co.jp>
2018-10-29 21:55:58 +0900

    IsGround関数だけ

---
+++ Assets/Script/Charactor/Player/Player_IsGround.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_IsGround : MonoBehaviour
+{
+    [HideInInspector]public bool IsGround;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        IsGround = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag != "Wall")
+            return;
+        IsGround = true;
+    }
+}
