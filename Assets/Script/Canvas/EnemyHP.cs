0bb5ccdc664fd99cac9e0e1bd29d12fcbc285e86
Bidoumyouou <moriyanotenagaasinagasama@yahoo.co.jp>
2018-10-29 12:36:43 +0900

    EnemyHPのUI作成

---
+++ Assets/Script/Canvas/EnemyHP.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHP : MonoBehaviour {
+
+
+    TestEnemy enemy;
+    Text text;
+
+    TestEnemyUI UI;
+	// Use this for initialization
+	void Start () {
+        UI = GetComponentInParent<TestEnemyUI>();
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemy == null && UI.TargetObject != null)
+            enemy = UI.TargetObject.GetComponent<TestEnemy>();
+        if (enemy != null)
+        {
+            text.text = "HP:" + enemy.status.HP.ToString();
+        }
+    }
+}
