1b5d9a1f6d7b6d8c6647e03058e021ef3cb32c5b
Bidoumyouou <moriyanotenagaasinagasama@yahoo.co.jp>
2018-11-01 18:13:43 +0900

    InputStackの実装

---
+++ Assets/Script/Charactor/Enemy/Equition/Eq_Timer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Eq_Timer : EqitionBase
+{
+    [Tooltip("条件を満たすための時間を表す")]public float Time  = 0;
+    [Tooltip("trueは↑の時間より後、falseは先")]public bool IsAfter = true;
+
+    public override bool Equition(Charactor _obj, ModeBase _mode)
+    {
+
+        if ((_obj.modetime > Time) & IsAfter )
+        {
+            return true;
+
+        }
+        return false;
+    }
+}
