fa8c759d09f00b7ec23d69ce3abbe65974cb4846
Bidoumyouou <moriyanotenagaasinagasama@yahoo.co.jp>
2018-11-11 23:15:51 +0900

    EnemyWallの作成

--- Assets/Script/Charactor/Enemy/E003_Gobrin/E003_Move.cs
+++ Assets/Script/Charactor/Enemy/E003_Gobrin/E003_Move.cs
@@ -3,6 +3,8 @@
 
 public class E003_Move : E_ModeBase
 {
+    [ToolTip("壁で方向転換するかどうか")]bool ReturnByWall = true;
+
     public float dash_speed;
     
     //プレイヤーを探す
@@ -53,5 +55,6 @@ public class E003_Move : E_ModeBase
         }
         */
     }
+
 
 }
