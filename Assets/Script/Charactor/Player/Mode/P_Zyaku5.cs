bb5440e6058ab2d435797a38e06d4d6c3b750561
Bidoumyouou <moriyanotenagaasinagasama@yahoo.co.jp>
2018-11-11 19:12:11 +0900

    5番目の攻撃をキックに変更

---
+++ Assets/Script/Charactor/Player/Mode/P_Zyaku5.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+
+public class P_Zyaku5 : P_ModeBase
+{
+
+
+    public int test;//祖父オブジェクトがMonoBehaviorを継承していてもインスペクタでは表示されない
+
+    public override void Mode_Start(Charactor _obj)
+    {
+
+        //アニメシグナルの呼び出し
+        player.ChangeAnimeSignal(16);
+        base.Mode_Start(_obj);
+        ////////////////////////
+        //以上、全アクション共通
+        ////////////////////////
+        //ひとつだけプレハブから攻撃オブジェクトを作成
+
+        //GameObject Enemy
+    }
+    public override void Mode_Update(Charactor _obj)
+    {
+        base.Mode_Update(_obj);
+        if (_obj.modetime > _obj.nowflag.StartTime && !ishitbox)
+        {
+            MakeHitBox(_obj, _obj.hitbox, 0,Attack[0]);
+        }
+
+
+  
+    }
+
+}
