23333c8e7b89e45efbd1c6a91378c78406a2fba3
Bidoumyouou <moriyanotenagaasinagasama@yahoo.co.jp>
2018-11-17 16:18:30 +0900

    サウンドリソースの確保

---
+++ Assets/Script/Charactor/Enemy/Boss/Boss.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss : TestEnemy
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        StartEnemy();
+        ChangeMode(3);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();
+
+    }
+}
